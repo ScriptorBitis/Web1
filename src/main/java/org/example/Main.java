3a5b1ee7ace602c3dbf967389cf5b7612a9c5e6e
Никита <podberezkin.n@mail.ru>
2025-09-19 18:36:19 +0300

    КОНЕЦ СТРАДАНИЯМ????

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Server server = new Server();
+            server.run();
+        } catch (IOException e) {
+            System.exit(0);
+        }
+    }
+
+}
